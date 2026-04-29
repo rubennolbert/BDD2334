989b6620f8cf3345c978ab374f6f2aa3a35b1f89
Ruben <rubenlopez9891@gmail.com>
2026-04-28 23:32:55 -0300

    1

---
+++ dos.js
@@ -0,0 +1,7 @@
+libros = [
+  {"titulo": "El Quijote", "autor": "Miguel de Cervantes"},
+  {"titulo": "Cien Años de Soledad", "autor": "Gabriel García Márquez"},
+  {"titulo": "La Sombra del Viento", "autor": "Carlos Ruiz Zafón"}
+];
+
+db.libros.insertMany(libros);
---
+++ uno.js
@@ -0,0 +1,30 @@
+let pepe = {
+  "nombre": "Jose",
+  "apellido": "Perez",
+  "edad": 30,
+  "profesion": "Desarrollador",
+  "soltero": true,
+  "hobbies": ["programar", "jugar videojuegos", "viajar"],
+  "direccion": {
+    "calle": "Calle Falsa",
+    "numero": 123,
+    "ciudad": "Ciudad Falsa"
+  },
+  "familia": [
+    {
+      "nombre": "Maria",
+      "relacion": "Hermana"
+    },
+    {
+      "nombre": "Carlos",
+      "relacion": "Padre"
+    }
+  ]
+}
+
+console.log(pepe.nombre);
+console.log(pepe.hobbies[0]);
+console.log(pepe.familia[0]);
+console.log(pepe.direccion.calle);
+
+
